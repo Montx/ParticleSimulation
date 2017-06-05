043265585ab1bb6c496b51d4afa336a295f00e80
ricardo.monteiro <ricardo.monteiro@miniclip.com>
2017-06-05 13:12:26 +0100

    Prepared Macro for XCode builds

---
+++ SDL_includes.h
@@ -0,0 +1,13 @@
+//
+//  SDL_includes.h
+//  ParticleSimulator
+//
+//  Created by Ricardo Monteiro on 05/06/17.
+//  Copyright © 2017 fatesoft. All rights reserved.
+//
+
+#ifdef MAC_OS
+#include <SDL2/SDL.h>
+#else
+#include <SDL.h>
+#endif
--- Screen.h
+++ Screen.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <SDL.h>
+#include "SDL_includes.h"
 
 namespace particleSim {
 
